5c8d450eb17e445e17dd11f607b681603157138f
siddhtyagi18 <siddhtyagi1845@gmail.com>
2025-12-07 18:46:25 +0530

    Add PingController with /ping endpoint

---
+++ backend/src/main/java/ai/turthcheck/backend/controller/PingController.java
@@ -0,0 +1,13 @@
+package ai.truthcheck.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
